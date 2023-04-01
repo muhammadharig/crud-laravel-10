ce5df00955e9b2b84a32541ce550a8bb799fb247
muhammadharig <mharigunawansyah@gmail.com>
2023-04-01 22:48:58 +0700

    membuat controller post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+// import Model Post
+use App\Models\Post;
+
+// return type View
+use Illuminate\View\View;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(): View
+    {
+        //get post
+        $posts = Post::latest()->paginate(5);
+
+        // render view with posts
+        return view('posts.index', compact('posts'));
+    }
 }
